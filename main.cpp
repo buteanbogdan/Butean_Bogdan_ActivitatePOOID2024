5ed2d74de43948fdbec33d39b67155d2df36ba6e
buteanbogdan <b.bogdan2302@gmail.com>
2024-11-16 13:13:40 +0200

    adaugare enum test

--- main.cpp
+++ main.cpp
@@ -1,1 +1,11 @@
 #include <stdio.h>
+#include <iostream>
+using namespace std;
+
+enum culoareSteag {albastru = 10, galben = 20, rosu = 30};
+
+int main() {
+	culoareSteag s1 = culoareSteag::galben;
+	cout << s1 << endl;
+	
+}
