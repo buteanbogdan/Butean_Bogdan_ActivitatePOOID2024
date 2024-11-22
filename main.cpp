ee1d8691ea4a4317a31ebc017796a8637b851880
buteanbogdan <b.bogdan2302@gmail.com>
2024-11-22 22:39:14 +0200

    Testare constructor implicit, constructor cu parametrii si destructor.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,36 @@
-#include <stdio.h>
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
+class Laptop {
+public:
+	int ram;
+	int storage;
+	int processor;
 
-enum culoareSteag {albastru = 10, galben = 20, rosu = 30};
 
-int main() {
-	culoareSteag s1 = culoareSteag::galben;
-	cout << s1 << endl;
-	
+	Laptop() {
+		ram = 0;
+		storage = 0;
+		processor = 0;
+	}
+
+	Laptop(int ram, int storage, int processor) {
+		this->ram = ram;
+		this->storage = storage;
+		this->processor = processor;
+	}
+	~Laptop() {
+	}
+};
+
+
+
+void main() {
+	Laptop laptop1;
+	cout << laptop1.ram << " " << laptop1.storage << " " << laptop1.processor << endl;
+	laptop1.ram = 8;
+	laptop1.storage = 256;
+	laptop1.processor = 2.4;
+	cout << laptop1.ram << " " << laptop1.storage << " " << laptop1.processor << endl;
+	Laptop laptop2(16, 512, 3.2);
+	cout << laptop2.ram << " " << laptop2.storage << " " << laptop2.processor << endl;
 }
